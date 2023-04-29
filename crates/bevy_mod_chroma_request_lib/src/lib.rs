6dba792333b6c816093d13bf5e348971ae1ba2fe
Darryl James <datael@datael.co.uk>
2023-04-29 11:54:19 +0900

    Build out initial API

---
+++ crates/bevy_mod_chroma_request_lib/src/lib.rs
@@ -0,0 +1,75 @@
+use bevy::{
+    ecs::system::SystemParam,
+    prelude::{Commands, Entity, Query, Res},
+};
+use bytes::Bytes;
+use plugin::{HttpRequest, HttpRequestClient, HttpResponseReceived};
+use reqwest::{Client, RequestBuilder, StatusCode};
+
+mod plugin;
+
+#[derive(SystemParam)]
+pub struct HttpRequests<'w, 's> {
+    commands: Commands<'w, 's>,
+    client: Res<'w, HttpRequestClient>,
+    response_received_query: Query<'w, 's, &'static HttpResponseReceived>,
+}
+
+impl<'w, 's> HttpRequests<'w, 's> {
+    pub fn client(&self) -> &Client {
+        &self.client.client
+    }
+
+    pub fn request(&mut self, request: RequestBuilder) -> HttpRequestHandle {
+        HttpRequestHandle {
+            entity: self.commands.spawn(HttpRequest::new(request)).id(),
+        }
+    }
+
+    pub fn get_response(
+        &self,
+        handle: &HttpRequestHandle,
+    ) -> Option<&Result<HttpResponse, HttpRequestError>> {
+        if let Ok(response) = self.response_received_query.get(handle.entity) {
+            Some(&response.result)
+        } else {
+            None
+        }
+    }
+
+    pub fn dispose(&mut self, handle: HttpRequestHandle) {
+        self.commands.entity(handle.entity).despawn();
+    }
+}
+
+#[derive(Debug)]
+pub struct HttpRequestHandle {
+    entity: Entity,
+}
+
+#[derive(Debug)]
+pub struct HttpResponse {
+    body_bytes: Bytes,
+    status_code: StatusCode,
+}
+
+impl HttpResponse {
+    pub fn body_bytes(&self) -> &Bytes {
+        &self.body_bytes
+    }
+
+    pub fn status_code(&self) -> StatusCode {
+        self.status_code
+    }
+}
+
+#[derive(Debug)]
+pub enum HttpRequestError {
+    RequestFailed(reqwest::Error),
+}
+
+impl From<reqwest::Error> for HttpRequestError {
+    fn from(error: reqwest::Error) -> Self {
+        Self::RequestFailed(error)
+    }
+}
---
+++ crates/bevy_mod_chroma_request_lib/src/plugin.rs
@@ -0,0 +1,101 @@
+use bevy::{
+    prelude::{App, Commands, Component, Entity, Plugin, Query, Resource, Without},
+    tasks::AsyncComputeTaskPool,
+};
+use crossbeam_channel::Receiver;
+use reqwest::{Client, RequestBuilder};
+
+use crate::{HttpRequestError, HttpResponse};
+
+pub struct RequestPlugin;
+
+impl Plugin for RequestPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<HttpRequestClient>()
+            .add_system(system_execute_requests)
+            .add_system(system_gather_responses);
+    }
+}
+
+#[derive(Resource, Default)]
+pub(crate) struct HttpRequestClient {
+    pub(crate) client: Client,
+}
+
+#[derive(Component)]
+pub(crate) struct HttpRequest {
+    builder: Option<RequestBuilder>,
+}
+
+impl HttpRequest {
+    pub(crate) fn new(builder: RequestBuilder) -> Self {
+        Self {
+            builder: Some(builder),
+        }
+    }
+}
+
+#[derive(Component, Debug)]
+pub(crate) struct HttpRequestInProgress {
+    pub receiver: Receiver<Result<HttpResponse, HttpRequestError>>,
+}
+
+#[derive(Component, Debug)]
+pub(crate) struct HttpResponseReceived {
+    pub(crate) result: Result<HttpResponse, HttpRequestError>,
+}
+
+fn system_execute_requests(
+    mut commands: Commands,
+    mut query: Query<(Entity, &mut HttpRequest), Without<HttpRequestInProgress>>,
+) {
+    for (entity, mut request) in query.iter_mut() {
+        let (sender, receiver) = crossbeam_channel::bounded(1);
+
+        let mut request_builder = None;
+        std::mem::swap(&mut request.builder, &mut request_builder);
+
+        // SAFETY: This system is only invoked when a request builder is not
+        // already in progress, and we only allow construction of a request by
+        // passing in a request builder instance, so this will never be None.
+        let request_builder = request_builder.unwrap();
+
+        AsyncComputeTaskPool::get()
+            .spawn(async move {
+                async fn run_request(
+                    request: RequestBuilder,
+                ) -> Result<HttpResponse, HttpRequestError> {
+                    let response = request.send().await?;
+                    let status_code = response.status();
+
+                    Ok(HttpResponse {
+                        status_code,
+                        body_bytes: response.bytes().await?,
+                    })
+                }
+
+                let result = run_request(request_builder).await;
+                sender.send(result).expect("sent successfully");
+            })
+            .detach();
+
+        commands
+            .entity(entity)
+            .insert(HttpRequestInProgress { receiver })
+            .remove::<HttpRequest>();
+    }
+}
+
+fn system_gather_responses(
+    mut commands: Commands,
+    query: Query<(Entity, &HttpRequestInProgress), Without<HttpResponseReceived>>,
+) {
+    for (entity, in_progress) in query.iter() {
+        if let Ok(result) = in_progress.receiver.try_recv() {
+            commands
+                .entity(entity)
+                .insert(HttpResponseReceived { result })
+                .remove::<HttpRequestInProgress>();
+        }
+    }
+}
