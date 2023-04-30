af371696549268ef229ed9f7f042c1d7a63d37ee
Darryl James <datael@datael.co.uk>
2023-04-30 11:52:14 +0900

    Change BGRColor to be a tuple struct
    Serialization is easier this way, plus it only has one field

--- crates/bevy_mod_chroma_api/src/bgr_color.rs
+++ crates/bevy_mod_chroma_api/src/bgr_color.rs
@@ -2,9 +2,7 @@ use bevy::prelude::Color;
 use serde::Serialize;
 
 #[derive(Debug, Serialize, Clone, Copy)]
-pub struct BGRColor {
-    color: u32,
-}
+pub struct BGRColor(u32);
 
 impl From<Color> for BGRColor {
     fn from(color: Color) -> Self {
@@ -12,8 +10,6 @@ impl From<Color> for BGRColor {
         let g = (color.g() * 255.0) as u32;
         let b = (color.b() * 255.0) as u32;
 
-        Self {
-            color: (b << 16) | (g << 8) | r,
-        }
+        Self((b << 16) | (g << 8) | r)
     }
 }
