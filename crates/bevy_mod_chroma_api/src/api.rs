2d8cd262dc24a54254312592cb3d913cb3d811a9
Darryl James <datael@datael.co.uk>
2023-04-30 11:53:16 +0900

    Define MouseEffect

--- crates/bevy_mod_chroma_api/src/api.rs
+++ crates/bevy_mod_chroma_api/src/api.rs
@@ -1,4 +1,23 @@
-use serde::Deserialize;
+use crate::bgr_color::BGRColor;
+use serde::{Deserialize, Serialize};
+
+// https://assets.razerzone.com/dev_portal/REST/html/md__r_e_s_t_external_04_mouse.html
+#[derive(Debug, Serialize)]
+#[serde(tag = "effect", content = "param")]
+pub enum MouseEffect {
+    #[serde(rename(serialize = "CHROMA_NONE"))]
+    None,
+    #[serde(rename(serialize = "CHROMA_STATIC"))]
+    Static {
+        #[serde(flatten)]
+        color: BGRColor,
+    },
+    #[serde(rename(serialize = "CHROMA_CUSTOM2"))]
+    Custom([[BGRColor; 7]; 9]),
+}
+
+#[derive(Debug, Serialize)]
+struct CreateMouseEffectRequest {}
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct SessionInfo {
