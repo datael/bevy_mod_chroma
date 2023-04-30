b1374ff22854289af2973b4669a1d9514c573403
Darryl James <datael@datael.co.uk>
2023-04-30 17:05:12 +0900

    Remove all _old files

--- _old/src/main.rs
+++
@@ -1,9 +0,0 @@
-use bevy::{prelude::App, DefaultPlugins};
-use rust_bevy_chroma::ChromaPlugin;
-
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(ChromaPlugin)
-        .run();
-}
