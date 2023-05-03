c055e4846dd5f62d9b1165f2905acc2029a7cbac
Darryl James <datael@datael.co.uk>
2023-05-03 19:06:45 +0900

    Implement From<BGRColor> for KeyColor

--- crates/bevy_mod_chroma_api/src/key_color.rs
+++ crates/bevy_mod_chroma_api/src/key_color.rs
@@ -6,20 +6,32 @@ use crate::bgr_color::BGRColor;
 #[derive(Debug, Serialize, Clone, Copy)]
 pub struct KeyColor(u32);
 
-impl From<Color> for KeyColor {
-    fn from(color: Color) -> Self {
+impl From<BGRColor> for KeyColor {
+    fn from(color: BGRColor) -> Self {
         const KEY_COLOR_MASK: u32 = 0x0100_0000;
-        let color_value = BGRColor::from(color).as_u32();
+        let color_value = color.as_u32();
 
         Self(KEY_COLOR_MASK | color_value)
     }
 }
 
-impl From<Option<Color>> for KeyColor {
-    fn from(maybe_color: Option<Color>) -> Self {
+impl From<Option<BGRColor>> for KeyColor {
+    fn from(maybe_color: Option<BGRColor>) -> Self {
         match maybe_color {
             Some(color) => Self::from(color),
             None => Self(0),
         }
+    }
+}
+
+impl From<Color> for KeyColor {
+    fn from(color: Color) -> Self {
+        Self::from(BGRColor::from(color))
+    }
+}
+
+impl From<Option<Color>> for KeyColor {
+    fn from(maybe_color: Option<Color>) -> Self {
+        Self::from(maybe_color.map(|color| BGRColor::from(color)))
     }
 }
